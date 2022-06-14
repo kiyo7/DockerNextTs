b71db8ea8e2f93de37a5d63ff19731396569c418
kiyo <kiyo324783@icloud.com>
2022-06-15 01:10:45 +0900

    初期設定ページデザイン作成

--- front/components/atom/Header.tsx
+++ front/components/atom/Header.tsx
@@ -3,12 +3,14 @@ import Image from 'next/image'
 
 //image
 import logo from '../../images/headerLogo.png'
+import { supabase } from '../../utils/supabase'
 
 export const Header = () => {
   return (
     <nav className="flex flex-wrap items-center justify-between bg-teal-500 p-4">
       <div className="mr-6 flex flex-shrink-0 items-center text-white">
-        <Image src={logo} width={40} height={40} />
+        <Image src={logo} width={40} height={40} onClick={() => supabase.auth.signOut()} />
+        {/* <-仮 */}
         <span className="text-xl font-semibold tracking-tight">Shifty</span>
       </div>
     </nav>
