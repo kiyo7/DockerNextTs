da05a73aa6a7ab65cab4af44c60e948d89dff084
kiyo <kiyo324783@icloud.com>
2022-06-21 12:07:04 +0900

    fix

--- front/pages/updateProf/[uid].tsx
+++ front/pages/updateProf/[uid].tsx
@@ -3,12 +3,11 @@ import { NextPage } from 'next'
 
 //components
 import { Layout } from '../../components/Layout'
-import { EditProfile } from '../../components/EditProfile'
 
 const ProfileUpdate: NextPage = () => {
   return (
     <Layout title="プロフィール編集">
-      <EditProfile />
+      <div>仮</div>
     </Layout>
   )
 }
